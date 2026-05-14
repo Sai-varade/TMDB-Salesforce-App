6ef6c18499ee653aedadd0848a0e2e1e677407e1
Sai-varade <Saivarade500@gmail.com>
2026-05-14 20:10:10 +0530

    Added TMDB home UI with movie card grid and pagination using SOQL OFFSET

---
+++ force-app/main/default/lwc/tmdbhome/tmdbhome.js
@@ -0,0 +1,42 @@
+import { LightningElement, wire, track } from 'lwc';
+
+import getmoviesss from '@salesforce/apex/movieList.getMov';
+import getLengthOfMovies from '@salesforce/apex/moviesLength.getLengthOfMovies';
+
+export default class Tmdbhome extends LightningElement {
+
+    maxPageNo = 0;
+    PageNo = 1;
+    @track movies = [];
+    Pagesize = 8;
+    previous = "< Previous";
+    next = "Next >";
+    
+    @wire(getLengthOfMovies)wiredPageLengthb({data}){
+    if(data){
+        this.maxPageNo = Math.ceil(data / this.Pagesize);
+        }
+    }
+  
+    @wire(getmoviesss,{PageNo : '$PageNo',Pagesize : '$Pagesize'})
+    wiredMovies({ data}) {
+
+        if(data) {
+            this.movies = data;
+        } 
+    }
+
+    handleclickNext(){
+        if(this.PageNo < this.maxPageNo){
+        this.PageNo = this.PageNo + 1;
+        }
+    }
+
+    handleclickPrevious(){
+        
+        if(this.PageNo > 1){
+            this.PageNo = this.PageNo - 1;
+        }
+        
+    }
+}
