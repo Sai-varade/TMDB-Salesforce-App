4caa9b91d63b69a12866b9e74a767b50e0066240
Sai-varade <Saivarade500@gmail.com>
2026-05-22 15:32:29 +0530

    Implemented Custom Record Page with poster and Embeded Trailer Video

--- force-app/main/default/lwc/aiSummaryC/aiSummaryC.js
+++ force-app/main/default/lwc/aiSummaryC/aiSummaryC.js
@@ -1,39 +1,8 @@
 import { LightningElement , api,wire} from 'lwc';
 
 
-import getSummary from '@salesforce/apex/movieSummryController.getSummary';
-import {getRecord} from 'lightning/uiRecordApi';
-
-import Summary__c from '@salesforce/schema/Movie__c.Summary__c';
-
 export default class AiSummaryC extends LightningElement {
 
-
-    @api recordId;
-
-    Summary = 'Loading summary...';
-
-    @wire(getRecord, { recordId: '$recordId', fields: [Summary__c] })
-    wiredRecord({ error, data }) {
-        if (data) {
-            
-            this.Summary = data.fields.Summary__c.value;
-
-        } else if (error) {
-
-            this.Summary = 'Failed to load summary wire.';
-        }
-    }   
-
-
-    handleRefresh(){
-
-        getSummary({recordId: this.recordId}).then(result => {
-            
-            this.Summary = result;
-        }).catch(error => {
-            this.Summary = 'Failed to load summary imperrative. Error: ' + error.body.message;
-        });
-    
-    }
 }
+
+
